7f41d735c6a5dc6b49adc7c2a447bfda943ae132
Josue Jimenez <jjimenez@siccob.com.mx>
2021-11-01 19:27:24 -0600

    First commit

---
+++ src/components/AddCategory.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState } from 'react'
+import PropTypes from 'prop-types';
+
+export const AddCategory = ({ setCategories }) => {
+
+    const [InputValue, setInputValue] = useState("");
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    }
+
+    // El form manda el evento a "handleSubmit"
+    const handleSubmit = (e) => {
+        // Prevenir el comportamiento por defecto del formiulario
+        e.preventDefault();
+
+        if (InputValue.trim().length > 2) {
+            setCategories(cats => [InputValue, ...cats]);
+            setInputValue(""); // Limia el input
+
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={InputValue}
+                    onChange={handleInputChange}
+                />
+            </form>
+        </>
+    )
+}
+
+
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
+}
+
